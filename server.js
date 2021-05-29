26f27a10f20875b5a4c27198c58efa03a5595db8
Ionescu-Ioan <ionescuionut3110@gmail.com>
2021-05-29 16:55:15 +0300

    final etapa 4

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const { exec } = require("child_process");
+const ejs = require('ejs');
+const session = require('express-session');
+const formidable = require('formidable');
+const nodemailer = require("nodemailer");
+const sassMiddleware = require('node-sass-middleware');
 
 // https://expressjs.com/en/guide/routing.html
 const express = require('express');
@@ -11,6 +17,19 @@ const app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// app.use(sassMiddleware({
+//     /* Options */
+//     dest: path.join(__dirname, '/media'),
+//     src: __dirname,
+//     prefix: '/css',
+//     debug: true
+// }));
+
+app.get("*/galerie_statica.json",function(req, res){    
+    console.log("intra!");
+    res.status(403).render("pagini/403");
+});
 
 // declare static directory -> https://expressjs.com/en/starter/static-files.html
 app.use('/media', express.static(__dirname + '/media'));
@@ -23,18 +42,20 @@ const imaginiVerificate = (function verificaImagini(){
     const vectImagini=[];
 
 	for (let im of jsi.imagini){
-		const imVeche= path.join(caleGalerie, im.fisier);//obtin claea completa (im.fisier are doar numele fisierului din folderul caleGalerie)		
-        const ext = path.extname(im.fisier);//obtin extensia
-		const numeFisier =path.basename(im.fisier,ext)//obtin numele fara extensie
+		const imVeche= path.join(caleGalerie, im.cale_fisier);//obtin claea completa (im.cale_fisier are doar numele fisierului din folderul caleGalerie)		
+        const ext = path.extname(im.cale_fisier);//obtin extensia
+		const numeFisier =path.basename(im.cale_fisier,ext)//obtin numele fara extensie
 		const imMica=path.join(caleGalerie+"/mic/", numeFisier+"-mic"+".webp");//creez cale apentru imaginea noua; prin extensia wbp stabilesc si tipul ei
 		const imMedie=path.join(caleGalerie+"/mediu/", numeFisier+"-mediu"+".webp");
         
         vectImagini.push({
-            mare: path.join(caleImagine, im.fisier), 
-            medie: path.join(caleImagine + "/mediu", im.fisier), 
-            mic: path.join(caleImagine + "/mic", im.fisier), 
-            anotimp: im.anotimp, 
-            descriere: im.descriere
+            mare: path.join(caleImagine, im.cale_fisier), 
+            medie: path.join(caleImagine + "/mediu", numeFisier+"-mediu"+".webp"), 
+            mic: path.join(caleImagine + "/mic", numeFisier+"-mic"+".webp"), 
+            titlu: im.titlu,
+            descriere: im.text_descriere,
+            anotimp: im.anotimp,
+            copyright: im.copyright
         }); //adauga in vector un element
     
 		
@@ -63,18 +84,83 @@ const imaginiVerificate = (function verificaImagini(){
     return vectImagini;
 })();
 
+app.get(["*/galerie_animata_4.css", "*/galerie_animata_9.css", "*/galerie_animata_16.css"],function(req, res){
+    const cssFileName = req.path.slice(11);
+    const scssFileName = cssFileName.slice(0, -4) + ".scss";
+    /*Atentie modul de rezolvare din acest app.get() este strict pentru a demonstra niste tehnici
+    si nu pentru ca ar fi cel mai eficient mod de rezolvare*/
+    res.setHeader("Content-Type","text/css");//pregatesc raspunsul de tip css
+    let sirScss=fs.readFileSync("./media/scss/" + scssFileName).toString("utf-8");//citesc scss-ul cs string
+    culori=["navy","black","purple","grey"]
+    let culoareAleatoare =culori[Math.floor(Math.random()*culori.length)];//iau o culoare aleatoare pentru border
+    let rezScss=ejs.render(sirScss,{culoare:culoareAleatoare});// transmit culoarea catre scss si obtin sirul cu scss-ul compilat
+    console.log(rezScss);
+    fs.writeFileSync("./temp/" + scssFileName,rezScss);//scriu scss-ul intr-un fisier temporar
+    exec(`sass ./temp/${scssFileName} ./temp/${cssFileName}`, (error, stdout, stderr) => {//execut comanda sass (asa cum am executa in cmd sau PowerShell)
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.end();//termin transmisiunea in caz de eroare
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            res.end();
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        //totul a fost bine, trimit fisierul rezultat din compilarea scss
+        res.sendFile(path.join(__dirname,`temp/${cssFileName}`));
+    });
+
+});
+
+//vooi afla anotimpul curent
+const anotimpuri={
+    iarna: [0, 1, 11],
+    primavara: [2, 3, 4],
+    vara: [5, 6, 7],
+    toamna: [8, 9, 10],
+}
+
+function obtineImaginiAnotimp(luna){
+    //Object.keys() imi intoarce un array cu toate cheile obiectului anotimpuri
+    const anotimpCurent=Object.keys(anotimpuri).find(val => anotimpuri[val].indexOf(luna) >=0);
+    //cu slice trunchiez numarul de imagini care se incadreaza in anotimpul curent la 10
+    return imaginiVerificate.filter(val => val.anotimp === anotimpCurent).slice(0, 10);
+}
 
 // index page
 app.get(['/', '/index'], function(req, res) {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip);
-    res.render('pagini/index', {galerie: imaginiVerificate, user_ip: ip});
+    const luna = new Date().getMonth();
+    const galerie = obtineImaginiAnotimp(luna);
+    const user_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.render('pagini/index', {galerie, user_ip});
 });
 
 
 app.get(['/pagina_galerie_statica'], function(req, res) {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    res.render('pagini/pagina_galerie_statica', {galerie: imaginiVerificate, user_ip: ip});
+    const luna = new Date().getMonth();
+    const galerie = obtineImaginiAnotimp(luna);
+    res.render('pagini/pagina_galerie_statica', {galerie});
+});
+
+app.get(['/pagina_galerie_animata'], function(req, res) {
+    const count = imaginiVerificate.length;
+    const getRand = (max) => Math.floor(Math.random() * max); 
+    const indexes = new Set();
+    const vector_dimensiune_grid = [4, 9, 16];
+    const nr_imagini = vector_dimensiune_grid[getRand(vector_dimensiune_grid.length)];
+
+    // console.log(nr_imagini);
+    while(indexes.size < nr_imagini)
+    {
+        const nr = getRand(count);
+        if(imaginiVerificate[nr].titlu.length < 12 )
+            indexes.add(nr);
+        // indexes.add(getRand(count));
+    }
+    const galerie = imaginiVerificate.filter((val, key) => indexes.has(key));
+    res.render('pagini/pagina_galerie_animata', {galerie});
 });
 
 // app.all('/media/*', function(req, res) {
@@ -104,21 +190,7 @@ app.get(['/:template', '/:template/*'], function(req, res, next) {
 //     next() // pass control to the next handler
 //   });
 
-app.get("/media",function(req, res){    
-    console.log("intra!");
-    res.render("pagini"+req.url, function(err,rezultatRandare){
-        if(err){
-            if(err.message.includes("Failed to lookup view")){
-                res.status(403).render("pagini/403");
-            }
-            else 
-                throw err;
-        }
-        else{
-            res.send(rezultatRandare);
-        }
-    });
-});
+
 
 // 404 status -> https://expressjs.com/en/starter/faq.html
 /*app.use(function (req, res, next) {
